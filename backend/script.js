ad72079cca4c5abece907f829aaee6653490cf57
Ted Breedon <breedon.ted@gmail.com>
2024-03-21 00:19:23 -0700

    New updates.

--- backend/script.js
+++ backend/script.js
@@ -1,1 +1,21 @@
 console.log('Hello World.')
+
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitCount();
+})
+
+const functionAPI = '';
+
+const getVisitCount = () => {
+    let count = 0
+    fetch(functionAPI).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
