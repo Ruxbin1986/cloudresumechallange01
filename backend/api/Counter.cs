06107440817dc346aba61b1ef9bf528357d7c01e
Ted Breedon <breedon.ted@gmail.com>
2024-03-22 04:41:39 -0700

    Added AzureWebJobsSecretStorageType back to local settings... How was it removed?

--- backend/api/Counter.cs
+++ backend/api/Counter.cs
@@ -1,13 +1,23 @@
+// Include the Newtonsoft.Json library to enable JSON serialization and deserialization features.
 using Newtonsoft.Json;
 
+// Declare a namespace for the current code. This helps in organizing and grouping related classes and functions.
 namespace Company.Function
 {
+    // Define a public class named 'Counter'. This class represents a model for counting something, 
+    // with each instance holding an ID and a count value.
     public class Counter
     {
-        [JsonProperty(PropertyName ="id")]
-        public string Id {get; set;}
+        // Apply the JsonProperty attribute to the Id property.
+        // This specifies the name of the property as it should appear in JSON when the object is serialized or deserialized.
+        // Here, it ensures the JSON property name for 'Id' is always lowercase "id".
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
         
-        [JsonProperty(PropertyName ="count")]
-        public int Count {get; set;}
+        // Apply the JsonProperty attribute to the Count property.
+        // This specifies the name of the property as it should appear in JSON when the object is serialized or deserialized.
+        // Here, it ensures the JSON property name for 'Count' is always lowercase "count".
+        [JsonProperty(PropertyName = "count")]
+        public int Count { get; set; }
     }
 }
